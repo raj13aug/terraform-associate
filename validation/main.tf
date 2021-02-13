7e2e0e253ceef009aeb5008d0389460a2902f8c6
Nataraj.R <raj13aug@gmail.com>
2021-02-13 19:23:26 +0530

    added

--- validation/main.tf
+++ validation/main.tf
@@ -17,6 +17,15 @@ variable "type" {
   }
 }
 
+variable "os" {
+  default = "linux"
+
+  validation {
+    # The condition here identifies if the variable contains the string "linxu" OR "windows".
+    condition = can(regex("linux|windows", var.os))
+    error_message = "ERROR: Operating System must be Windows OR Linux."
+  }
+}
 
 resource "aws_instance" "example" {
   ami           = var.AMI_image
